3d8c1e1be13fe2e973042f58c351a34ddea300ba
amaevsky <maevsky.artem@gmail.com>
2021-07-07 11:14:12 -0500

    use swagger in prod

--- api/Lingua/Lingua.API/Startup.cs
+++ api/Lingua/Lingua.API/Startup.cs
@@ -64,12 +64,12 @@ namespace Lingua.API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            //if (env.IsDevelopment())
+           // {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Lingua.API v1"));
-            }
+           // }
 
             app.UseHttpsRedirection();
 
