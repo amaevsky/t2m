4849597c6097b3870e48d20ee37f8f7f556a17b8
amaevsky <maevsky.artem@gmail.com>
2021-07-09 18:31:37 -0500

    feat: add signalr

---
+++ api/Lingua.API/Realtime/RoomsHub.cs
@@ -0,0 +1,26 @@
+﻿using Lingua.Shared;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Lingua.API.Realtime
+{
+
+    public interface IRoomsRealtimeClient
+    {
+        Task OnAdd(Room room, Guid? by);
+        Task OnUpdate(Room room, Guid? by);
+        Task OnEnter(Room room, Guid? by);
+        Task OnLeave(Room room, Guid? by);
+        Task OnRemove(Room room, Guid? by);
+    }
+
+    public class RoomsHub : Hub<IRoomsRealtimeClient>
+    {
+
+    }
+}
+
+
--- api/Lingua.Shared/Rooms/Room.cs
+++ api/Lingua.Shared/Rooms/Room.cs
@@ -16,6 +16,7 @@ namespace Lingua.Shared
         public string Language { get; set; }
         public Guid? HostUserId { get; set; }
         public string JoinUrl { get; set; }
+        public int MaxParticipants { get; set; }
         public List<User> Participants { get; set; }
 
         public User Host => Participants.Find(p => p.Id == HostUserId);
