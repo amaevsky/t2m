0192f74e1df1f56b27f96e1d092f321f2a87f00b
amaevsky <maevsky.artem@gmail.com>
2021-07-14 15:12:09 -0500

    fix: use prod url for signalr

--- web/src/realtime/roomsHub.ts
+++ web/src/realtime/roomsHub.ts
@@ -1,5 +1,5 @@
 import * as signalR from '@microsoft/signalr';
-import { BASE_URL } from '../constants';
+import { BASE_URL } from '../utilities/http';
 
 export const connection = new signalR.HubConnectionBuilder()
   .withUrl(`${BASE_URL}/roomsHub`)
