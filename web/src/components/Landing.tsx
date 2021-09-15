34ba2b21ca2bf5f3ab18aaf644b7701984123491
amaevsky <maevsky.artem@gmail.com>
2021-09-15 16:47:44 -0500

    finilize tutorial on landing page

--- web/src/components/Landing.tsx
+++ web/src/components/Landing.tsx
@@ -1,18 +1,26 @@
-import { Col, Row, Button, Typography, Space } from "antd"
+import { Col, Row, Button, Typography, Space } from "antd";
 import { Redirect } from "react-router-dom";
 import { userService } from "../services/userService";
 import { routes } from "./App";
-import { Tile } from "./Tile";
 const { Title } = Typography;
 
 export const Landing = (props: any) => {
+
+  const signinBtn =
+    <Button
+      onClick={() => props.history.push(routes.login.login)} style={{ width: 150 }}
+      type='primary'
+      size='large'>
+      Get Started
+    </Button>;
+
   return (
     <>
       {userService.isAccountReady &&
         <Redirect to={routes.app.findRoom} />
       }
       <Row align='middle'>
-        <Col md={12} xs={24} style={{ padding: '100px 0' }}>
+        <Col md={14} xs={24} style={{ padding: '100px 0' }}>
 
           <Row justify='center'>
             <Col span={18}>
@@ -27,36 +35,36 @@ export const Landing = (props: any) => {
                   <li>overcome the fear of speaking</li>
                   <li>save some money on tutors</li>
                 </ul>
-
-                <Button onClick={() => props.history.push(routes.login.login)} style={{ width: 150 }} type='primary' size='large'>Get Started</Button>
-
+                {signinBtn}
               </Space>
             </Col>
           </Row>
         </Col>
-        <Col md={12} xs={24}>
+        <Col md={10} xs={24}>
           <img src='/landing.png' width='100%'></img>
         </Col>
       </Row >
-      <Row style={{padding: '32 0'}} gutter={[32, 32]} className='primary-back' justify='center'>
-          <Row justify='center'>
-            <Title style={{ color: '#fff' }} level={2}>How it works</Title>
-          </Row>
-          <Row gutter={[32, 32]} justify='center'>
-            <Col lg={5} md={6} xs={16} >
-              <Tile style={{ padding: 16 }}><img src="/Tutorial - Step1.png" width='100%' /></Tile>
-            </Col >
-            <Col lg={5} md={6} xs={16}>
-              <Tile style={{ padding: 16 }}><img src="/Tutorial - Step2.png" width='100%' /></Tile>
-            </Col>
-            <Col lg={5} md={6} xs={16}>
-              <Tile style={{ padding: 16 }}><img src="/Tutorial - Step3.png" width='100%' /></Tile>
-            </Col>
-          </Row>
-          <Row justify='center'>
-            <Button type='default' className='primary-color' size='large' style={{ width: 150 }}>Get started</Button>
-          </Row>
-      </Row>
+
+      <Space direction='vertical' size={40}>
+        <Row justify='center'>
+          <Title level={3}>How it works</Title>
+        </Row>
+        <Row gutter={[32, 32]} justify='center'>
+          <Col lg={5} md={6} xs={16} >
+            <img src="/Tutorial - Step1.png" width='100%' />
+          </Col >
+          <Col lg={5} md={6} xs={16}>
+            <img src="/Tutorial - Step2.png" width='100%' />
+          </Col>
+          <Col lg={5} md={6} xs={16}>
+            <img src="/Tutorial - Step3.png" width='100%' />
+          </Col>
+        </Row>
+        <Row justify='center'>
+          {signinBtn}
+        </Row>
+        <div></div>
+      </Space>
 
     </>
   )
